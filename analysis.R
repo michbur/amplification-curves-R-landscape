401debe9f59b7ab4424619e00eb6a8361197212c
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2021-05-02 08:21:22 +0200

    finished plot

--- analysis.R
+++ analysis.R
@@ -3,6 +3,14 @@ library(pbapply)
 library(dplyr)
 library(reshape2)
 library(igraph)
+library(ggraph)
+library(grid)
+library(gridExtra)
+
+get_legend <- function(gg_plot) {
+  grob_table <- ggplotGrob(gg_plot)
+  grob_table[["grobs"]][[which(sapply(grob_table[["grobs"]], function(x) x[["name"]]) == "guide-box")]]
+}
 
 package_names <- readLines("./data/pcr-packages.txt")
 
@@ -44,4 +52,21 @@ node_df <- data.frame(package_name = package_names, not_on_cran = sapply(package
 pkg_graph <- graph_from_data_frame(edge_df, directed = TRUE, vertices = node_df)
 plot(pkg_graph)
 
+pkg_colors <- c("chartreuse3", "cornflowerblue", "orange")
+color_legend <- get_legend(ggplot(node_df, aes(x = 1, y = 1, fill = location)) +
+  geom_tile() +
+  scale_fill_manual("", values = pkg_colors) +
+  theme_bw() +
+  theme(legend.position = "bottom"))
 
+graph_plot <- ggraph(pkg_graph, layout = "kk") + 
+  geom_edge_link() + 
+  geom_node_label(aes(label = name, color = location), size = 3.2) +
+  scale_color_manual("", values = pkg_colors, guide = FALSE) +
+  theme_void()
+
+pdf("package_network.pdf", width = 9, height = 8)
+grid.arrange(graph_plot,
+             color_legend,
+             heights = c(0.9, 0.1))
+dev.off()
